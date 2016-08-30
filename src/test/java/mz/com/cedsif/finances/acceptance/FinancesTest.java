33e7178f0ad2dd008d59c8c41a0bd7f0b32ecf07
Amade Omar Ismael <amadeomarismael@hotmail.com>
2016-08-31 00:49:52 +0200

    início do ciclo tdd; escreveu-se o teste de aceitação (utilizando o selenium) para visualizar a página com os registos financeiros; este teste quebrou porque ainda não existe o código da página

---
+++ src/test/java/mz/com/cedsif/finances/acceptance/FinancesTest.java
@@ -0,0 +1,13 @@
+package mz.com.cedsif.finances.acceptance;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FinancesTest {
+
+	public static void main(String[] args) {		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://127.0.0.1:9994/finances/personal");
+	}
+
+}
